6e9706457a919ec2120fbd372e883d144ab1867c
Stefan Ninic <ninicstefan94@gmail.com>
2021-01-27 11:41:42 +0100

    Customer addresses

--- src/Magento.php
+++ src/Magento.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nts
- * Date: 31.3.18.
- * Time: 15.12
- */
 
 namespace KgBot\Magento;
 
+use KgBot\Magento\Builders\CustomerAddressBuilder;
 use KgBot\Magento\Builders\CustomerBuilder;
 use KgBot\Magento\Builders\CustomerGroupBuilder;
 use KgBot\Magento\Builders\OrderBuilder;
@@ -16,63 +11,64 @@ use KgBot\Magento\Utils\Request;
 
 class Magento
 {
-    /**
-     * @var $request Request
-     */
-    protected $request;
+	/**
+	 * @var $request Request
+	 */
+	protected $request;
 
-    /**
-     * Rackbeat constructor.
-     *
-     * @param null  $token   API token
-     * @param array $options Custom Guzzle options
-     * @param array $headers Custom Guzzle headers
-     */
-    public function __construct( $token = null, $options = [], $headers = [] )
-    {
-        $this->initRequest( $token, $options, $headers );
-    }
+	/**
+	 * Rackbeat constructor.
+	 *
+	 * @param null  $token   API token
+	 * @param array $options Custom Guzzle options
+	 * @param array $headers Custom Guzzle headers
+	 */
+	public function __construct( $token = null, $options = [], $headers = [] ) {
+		$this->initRequest( $token, $options, $headers );
+	}
 
-    /**
-     * @param       $token
-     * @param array $options
-     * @param array $headers
-     */
-    private function initRequest( $token, $options = [], $headers = [] )
-    {
-        $this->request = new Request( $token, $options, $headers );
-    }
+	/**
+	 * @param       $token
+	 * @param array $options
+	 * @param array $headers
+	 */
+	private function initRequest( $token, $options = [], $headers = [] ): void {
+		$this->request = new Request( $token, $options, $headers );
+	}
 
-    /**
-     * @return \KgBot\Magento\Builders\OrderBuilder
-     */
-    public function orders()
-    {
-        return new OrderBuilder( $this->request );
-    }
+	/**
+	 * @return OrderBuilder
+	 */
+	public function orders(): OrderBuilder {
+		return new OrderBuilder( $this->request );
+	}
 
-    /**
-     * @return \KgBot\Magento\Builders\CustomerBuilder
-     */
-    public function customers()
-    {
-        return new CustomerBuilder( $this->request );
-    }
+	/**
+	 * @return CustomerBuilder
+	 */
+	public function customers(): CustomerBuilder {
+		return new CustomerBuilder( $this->request );
+	}
 
-    /**
-     * @return \KgBot\Magento\Builders\ProductBuilder
-     */
-    public function products()
-    {
-        return new ProductBuilder( $this->request );
-    }
+	/**
+	 * @return CustomerAddressBuilder
+	 */
+	public function customer_addresses(): CustomerAddressBuilder {
+		return new CustomerAddressBuilder( $this->request );
+	}
 
-    /**
-     * @return \KgBot\Magento\Builders\CustomerGroupBuilder
-     */
-    public function customer_groups()
-    {
+	/**
+	 * @return ProductBuilder
+	 */
+	public function products(): ProductBuilder {
+		return new ProductBuilder( $this->request );
+	}
 
-        return new CustomerGroupBuilder( $this->request );
-    }
+	/**
+	 * @return CustomerGroupBuilder
+	 */
+	public function customer_groups(): CustomerGroupBuilder {
+
+		return new CustomerGroupBuilder( $this->request );
+	}
 }
--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -13,6 +13,34 @@ use KgBot\Magento\Utils\Model;
 
 class Customer extends Model
 {
-    protected $entity     = 'customers';
-    protected $primaryKey = 'id';
+	protected $entity     = 'customers';
+	protected $primaryKey = 'id';
+
+	public function getBillingAddress() {
+		return $this->request->handleWithExceptions( function () {
+
+			$address      = $this->request->client->get( "{$this->entity}/{$this->{$this->primaryKey}}/billingAddress" );
+			$responseData = json_decode( (string) $address->getBody() );
+
+			if ( ! empty( $responseData ) ) {
+				return new CustomerAddress( $this->request, $responseData );
+			}
+
+			return null;
+		} );
+	}
+
+	public function getShippingAddress() {
+		return $this->request->handleWithExceptions( function () {
+
+			$address      = $this->request->client->get( "{$this->entity}/{$this->{$this->primaryKey}}/shippingAddress" );
+			$responseData = json_decode( (string) $address->getBody() );
+
+			if ( ! empty( $responseData ) ) {
+				return new CustomerAddress( $this->request, $responseData );
+			}
+
+			return null;
+		} );
+	}
 }
